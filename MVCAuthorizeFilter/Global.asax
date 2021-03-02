018c2f5f86f5f3de3fd5c9daafe412e7b7d4402e
Ricardo Artola <rartola@outlook.com>
2021-03-02 13:20:54 -0500

    Add project files.

---
+++ MVCAuthorizeFilter/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAuthorizeFilter.MvcApplication" Language="C#" %>
---
+++ MVCAuthorizeFilter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAuthorizeFilter.Startup))]
+namespace MVCAuthorizeFilter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
